5e8f049c3dbdaee11bf1a2d03344110e46a524f7
Akash Menon <akashmenon@live.se>
2020-02-03 20:11:21 +0100

    MQwrite working, MQread needs fixing

--- Lab_1/src/Main.c
+++ Lab_1/src/Main.c
@@ -4,24 +4,40 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
 
 #define MQ_NAME "/our_mq"
 
-static mqd_t our_mq;
+static mqd_t our_mq, client_mq; //Message queue descriptors
 pthread_t sthread, cthread;
 
+
 void *server(void *arg){
+	char msg[20];
 	printf("%d\n", MQcreate(&our_mq, MQ_NAME));
-	printf("MQ status from server %s\n", strerror(errno));
+	printf("MQ creation status from server %s\n", strerror(errno));
 	while(1){
-		//TODO
+		fgets(&msg, 20, stdin);
+		printf("The message is: %s\n", msg);
+		printf("Write Attempt Code: %d\n", MQwrite(our_mq, msg));
+		printf("MQ write attempt from server:  %s\n", strerror(errno));
+		if(strcmp(msg,"END\n") == 0){ //fgets includes newline from input
+			printf("Exiting server thread.\n");
+			pthread_exit(&sthread);
+		}
+
 	}
 }
 void *client(void *arg){
-	printf("%d\n", MQconnect(&our_mq, MQ_NAME));
-	printf("MQ status from client  %s\n", strerror(errno));
+	sleep(1);
+	printf("%d\n", MQconnect(&client_mq, MQ_NAME));
+	printf("MQ connection status from client  %s\n", strerror(errno));
+	char rBuffer[2048];
 	while(1){
-		//TODO
+		sleep(1);
+		printf("Number of bytes read: %d\n", MQread(client_mq,&rBuffer));
+		printf("MQ read attempt from client:  %s\n", strerror(errno));
+		printf("Client: Msg received is %s\n", rBuffer);
 	}
 }
 
@@ -30,6 +46,7 @@ int main() {
 	pthread_create(&cthread, NULL,client, NULL);
 
 	pthread_join(sthread, NULL);
+	printf("Server exited.\n");
 	pthread_join(cthread, NULL);
 }
 
--- Lab_1/src/wrapper.c
+++ Lab_1/src/wrapper.c
@@ -1,7 +1,7 @@
 #include "wrapper.h"
 
 #define MAX_SIZE 1024 //msg size
-#define MAX_MESSAGES 10
+#define MAX_MESSAGES 100
 
 //Big note! If you think the arguments makes no sense, you are allowed to change them, as long as the basic functionality is kept
 //In case you run in to blocking issues with reading from the queue, the option O_NONBLOCK is a hot tip
@@ -9,11 +9,13 @@
 int MQcreate (mqd_t * mq, char * name)
 { 	//Should create a new messagequeue, use mq as reference pointer so you can reach the handle from anywhere
 	//Should return 1 on success and 0 on fail
-	struct mq_attr our_mq_attr;
-	our_mq_attr.mq_msgsize = MAX_SIZE;
-	our_mq_attr.mq_maxmsg = MAX_MESSAGES;
+	struct mq_attr attr;
+	attr.mq_flags = 0;
+	attr.mq_maxmsg = MAX_MESSAGES;
+	attr.mq_msgsize = MAX_SIZE;
+	attr.mq_curmsgs = 0;
 
-	mq = mq_open(name, O_CREAT|O_RDWR|O_NONBLOCK,0666, &our_mq_attr);
+	*mq = mq_open(name, O_CREAT|O_RDWR|O_NONBLOCK,0666, &attr);
 	if(mq != -1) return 1;
 	else return 0;
 
@@ -23,7 +25,7 @@ int MQconnect (mqd_t * mq, char * name)
     /* Connects to an existing mailslot for writing Uses mq as reference pointer, so that you can 	reach the handle from anywhere*/
     /* Should return 1 on success and 0 on fail*/
 
-	mq = mq_open(name, O_RDONLY);
+	*mq = mq_open(name, O_RDONLY);
 	if(mq != -1) return 1;
 	else return 0;
 }
@@ -33,13 +35,22 @@ void * MQread (mqd_t mq, void ** buffer)
 
     /* Read a msg from a mailslot, return nr Uses mq as reference pointer, so that you can 		reach the handle from anywhere */
     /* should return number of bytes read              */
+	int status;
+	status = mq_receive(mq, &buffer, strlen(buffer), 0);
+	return status;
+
 }
 
 int MQwrite (mqd_t mq, void * sendBuffer)
 {
     /* Write a msg to a mailslot, return nr Uses mq as reference pointer, so that you can 	     reach the handle from anywhere*/
     /* should return number of bytes read         */
-	return 1;
+	int status;
+	status = mq_send(mq, &sendBuffer, strlen(sendBuffer)+1, 0);
+
+	//TODO return no of bytes read?
+	if(status != -1) return 1;
+	else return 0;
 }
 
 int MQclose(mqd_t * mq, char * name)
