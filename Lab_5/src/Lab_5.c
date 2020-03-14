c061feeb541bc664d0012a8d77f658799bb98bfa
Edvin <edvinasmussen@gmail.com>
2020-03-14 15:52:11 +0100

    LFU, FIFO, OPT och egen algoritm

--- Lab_5/src/Lab_5.c
+++ Lab_5/src/Lab_5.c
@@ -6,6 +6,8 @@ typedef struct {
     int time;       // Time stamp of page stored in this memory frame
     int free;       // Indicates if frame is free or not
                     // Add own data if needed for FIFO, OPT, LFU, Own algorithm
+    int initTime;
+    int uses;
 } frameType;
 
 //---------------------- Initializes by reading stuff from file and inits all frames as free -----------------------------------------------------------
@@ -14,7 +16,7 @@ void initilize (int *no_of_frames, int *no_of_references, int refs[], frameType 
 
     int i;
     FILE *fp;
-    char fileName[50]="/home/student/Desktop/Labs_material/ref.txt";
+    char fileName[50]="./ref.txt";
 
     fp = fopen(fileName, "r");
 
@@ -73,17 +75,104 @@ void printResultOfReference (int no_of_frames, frameType frames[], int pf_flag, 
 }
 
 //----------- Finds the position in memory to evict in case of page fault and no free memory location ---------------------------------------------
+int OPT(frameType frames[], int n, int refCounter, int no_of_references, int refs[]){
+	int* countToNextUse = (int*)malloc(sizeof(int)*n); //Array av räknare. En räknare för varje frames sida
+	for(int x=0; x<n; x++){
+		countToNextUse[x]=0;
+	}
 
-int findPageToEvict(frameType frames[], int n) {   // LRU eviction strategy -- This is what you are supposed to change in the lab for LFU and OPT
-    int i, minimum = frames[0].time, pos = 0;
+	int pos = 0;
 
-    for(i = 1; i < n; ++i) {
-        if(frames[i].time < minimum){               // Find the page position with minimum time stamp among all frames
-            minimum = frames[i].time;
-            pos = i;
-        }
-    }
-    return pos;                                     // Return that position
+	for(pos = 0; pos<n; pos++){	//kolla för alla frames
+		for(int i = refCounter; i < no_of_references; i++){ //jämför page med alla element som kommer att komma refs[]
+			if(frames[pos].page != refs[i]){
+				countToNextUse[pos]++;
+			}
+			else i = no_of_references;//avrbryt loopen
+		}
+	}
+
+	pos = 0;
+	for(int j = 0; j<n; j++){
+		if(countToNextUse[j] > countToNextUse[pos]){
+			pos = j;
+		}
+	}
+	free(countToNextUse);
+	return pos;
+
+}
+
+
+int OWN(frameType frames[], int n, int counter, int no_of_references, int refs[]){//algoritmen behåller dem sidor som kommer användas mest i framtiden
+	int* pageFutureUses = (int*)malloc(sizeof(int)*n); //Array av räknare. En räknare för varje frame och hur många gånger dess sida kommer användas
+	for(int x=0; x<n; x++){
+		pageFutureUses[x]=0;
+	}
+
+	int pos = 0;
+
+	for(pos = 0; pos<n; pos++){	//kolla för alla frames
+		for(int i = counter; i < no_of_references; i++){
+			if(frames[pos].page == refs[i]){
+				pageFutureUses[pos]++;
+			}
+		}
+	}
+
+	pos = 0;
+	for(int j = 0; j<n; j++){
+		if(pageFutureUses[j] < pageFutureUses[pos]){
+			pos = j;
+		}
+	}
+	free(pageFutureUses);
+	return pos;
+
+}
+
+int LRU(frameType frames[], int n){		// LRU eviction strategy --
+	int i, minimum = frames[0].time, pos = 0;
+
+	    for(i = 1; i < n; ++i) {
+	        if(frames[i].time < minimum){               // Find the page position with minimum time stamp among all frames
+	            minimum = frames[i].time;
+	            pos = i;
+	        }
+	    }
+	    return pos;							// Return that position
+}
+
+int FIFO(frameType frames[], int n){
+	int firstIncome = 0;	//börja en räknare vid 0
+	int pos = 1;	//börja en annan räknare vid 1
+
+	for( pos = 1; pos < n; pos++){	//gå igenom alla frames
+		if(frames[pos].initTime < frames[firstIncome].initTime){	//om denna frame kom in tidigare
+			firstIncome = pos;	//
+		}
+	}
+	return firstIncome;
+}
+
+int LFU(frameType frames[], int n){
+	int leastUsedPos = 0;	//börja en räknare vid 0
+	int pos = 1;	//börja en annan räknare vid 1
+
+	for( pos = 1; pos < n; pos++){	//gå igenom alla frames
+		if(frames[pos].uses < frames[leastUsedPos].uses){	//om denna frame blivit mindre använd..
+			leastUsedPos = pos;	//byt till minst använda frames position
+		}
+	}
+	return leastUsedPos;	//skicka tillbaka första platsen som är minst oanvänd
+}
+
+int findPageToEvict(frameType frames[], int n, int counter, int no_of_references, int refs[]) {   //This is what you are supposed to change in the lab for LFU and OPT
+    //return LRU(frames, n);                                   // Return that position
+    //return LFU(frames, n);	//Least Frequently Used
+    //return FIFO(frames, n);	//First In First Out
+    //return OWN(frames, n, counter, no_of_references, refs);	//OWN algorithm
+    return OPT(frames, n, counter, no_of_references, refs);	//OPT
 }
 
 //---- Main loops ref string, for each ref 1) check if ref is in memory, 2) if not, check if there is free frame, 3) if not, find a page to evict --
@@ -101,6 +190,7 @@ int main()
             if(frames[j].page == refs[i]) {         // Accessed ref is in memory
                 counter++;
                 frames[j].time = counter;           // Update the time stamp for this frame
+                frames[j].uses = frames[j].uses+1;//uppdatera antalet gånger denna page använts sen den sattes i en frame
                 page_fault_flag = no_free_mem_flag = 1; // Indicate no page fault (no page fault and no free memory frame needed)
                 free = -1;                          // Indicate no free mem frame needed (reporting purposes)
                 break;
@@ -115,6 +205,8 @@ int main()
                     frames[j].page= refs[i];        // Update memory frame with referenced page
                     frames[j].time = counter;       // Update the time stamp for this frame
                     frames[j].free = 0;             // This frame is no longer free
+                    frames[j].uses = 1;//uppdatera antalet gånger denna page använts sen den sattes i en frame
+                    frames[j].initTime = counter;//uppdatera när denna page sattes i en frame
                     no_free_mem_flag = 1;           // Indicate that we do not need to replace since free frame was found
                     free = j;                       // Inicate that we found position j as free (reporting purposes)
                     break;
@@ -123,11 +215,13 @@ int main()
         }
 
         if(no_free_mem_flag == 0) {                 // Page fault and memory is full, we need to know what page to evict
-            pos = findPageToEvict(frames, no_of_frames); // Get memory position to evict among all frames
+            pos = findPageToEvict(frames, no_of_frames, counter, no_of_references, refs); // Get memory position to evict among all frames
             counter++;
             faults++;
             frames[pos].page = refs[i];             // Update memory frame at position pos with referenced page
             frames[pos].time = counter;             // Update the time stamp for this frame
+            frames[pos].uses = 1;	//uppdatera antalet gånger denna page använts sen den sattes i en frame
+            frames[pos].initTime = counter;	//uppdatera när denna page sattes i en frame
         }
         printResultOfReference (no_of_frames, frames, page_fault_flag, no_free_mem_flag, pos, free, refs[i]); // Print result of referencing ref[i]
     }
